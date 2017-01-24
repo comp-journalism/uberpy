645fe3888d727efc73e241d387859686947f36ea
Nick Diakopoulos <nicholas.diakopoulos@gmail.com>
2017-01-24 10:51:00 -0500

    Fixed issue with collation of asynchronous API calls.

--- gatherUberData.py
+++ gatherUberData.py
@@ -1,16 +1,17 @@
 import ConfigParser
 import json
 import time
-import datetime
 import grequests
 import sys
 import os
 import csv
 import urllib
 import logging
+import pytz
+from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 
-# The API end points 
+# The API end points
 price_url = "https://api.uber.com/v1/estimates/price"
 product_url = "https://api.uber.com/v1/products"
 time_url = "https://api.uber.com/v1/estimates/time"
@@ -32,7 +33,7 @@ fileWriter.writerow(["timestamp", "surge_multiplier", "expected_wait_time", "pro
 # These api param objects are used to send requests to the API, we create api_param objects for each location and endpoint we want to gather
 api_params = []
 for l in locations:
-	location_id = l["location_id"]
+	location_id = l["locations"]
 	price_parameters = {
 		'start_latitude': l["latitude"],
 		'end_latitude': l["latitude"],
@@ -42,11 +43,112 @@ for l in locations:
 
 	time_parameters = {
 		'start_latitude': l["latitude"],
-		'start_longitude': l["longitude"],		
+		'start_longitude': l["longitude"],
 	}
-	
+
 	api_params.append({"url": price_url, "location_id": location_id, "type": "price", "parameters": price_parameters})
 	api_params.append({"url": time_url, "location_id": location_id, "type": "time", "parameters": time_parameters})
+
+# Create local time zone for DC
+local_tz = pytz.timezone('US/Eastern')
+
+def utc_to_local(utc_dt):
+    return utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
+
+def aslocaltimestr(utc_dt):
+    return utc_to_local(utc_dt).strftime('%Y-%m-%d %H:%M:%S')
+
+
+tokennum = 0
+def gather_loop():
+	global tokennum
+	# A list to hold our things to do via async
+	async_action_items = []
+
+	# need one dictionary for every pair of requests
+	common_data_dicts = []
+	for i in range (0,len(api_params) / 2):
+		common_data_dicts.append({})
+
+	for i, api_param in enumerate(api_params):
+		# Get the current time
+		api_param["datetime"] = aslocaltimestr(datetime.utcnow())
+		api_param["data"] = common_data_dicts[(int)(i / 2)]
+		api_param["parameters"]["server_token"] = uber_server_tokens[tokennum % len(uber_server_tokens)]
+		# From here: http://stackoverflow.com/questions/25115151/how-to-pass-parameters-to-hooks-in-python-grequests
+		action_item = grequests.get(api_param["url"]+"?"+urllib.urlencode(api_param["parameters"]), hooks={'response': [hook_factory(api_param)]})
+		async_action_items.append(action_item)
+		# increment the token num so that we use the next server key next time
+		tokennum = tokennum + 1
+
+	# Initiate both requests in parallel
+	grequests.map(async_action_items)
+
+
+def hook_factory(*factory_args, **factory_kwargs):
+	def it_responded(res, **kwargs):
+		call_type = factory_args[0]["type"]
+		location_id = factory_args[0]["location_id"]
+		current_time = factory_args[0]["datetime"]
+		data_dict = factory_args[0]["data"]
+		try:
+			json_response = json.loads(res.content)
+			#print json_response
+			# Parse the data differently depending on the type of call it was
+			try:
+				if call_type == "time":
+					for t in json_response["times"]:
+						if t["display_name"] not in data_dict:
+							data_dict[t["display_name"]] = {}
+						data_dict[t["display_name"]]["expected_wait_time"] = t["estimate"]
+				elif call_type == "price":
+					for p in json_response["prices"]:
+						if p["display_name"] not in data_dict:
+							data_dict[p["display_name"]] = {}
+
+						data_dict[p["display_name"]]["surge_multiplier"] = p["surge_multiplier"]
+						data_dict[p["display_name"]]["product_type"] = p["display_name"]
+						data_dict[p["display_name"]]["low_estimate"] = p["low_estimate"]
+						if data_dict[p["display_name"]]["low_estimate"] != None:
+							data_dict[p["display_name"]]["low_estimate"] = int(data_dict[p["display_name"]]["low_estimate"])
+
+						data_dict[p["display_name"]]["high_estimate"] = p["high_estimate"]
+						if data_dict[p["display_name"]]["high_estimate"] != None:
+							data_dict[p["display_name"]]["high_estimate"] = int(data_dict[p["display_name"]]["high_estimate"])
+
+				#print data_dict
+				# For each product in the data dict write out a row
+				for p in data_dict:
+					data_dict[p]["timestamp"] = current_time
+					data_dict[p]["start_location_id"] = location_id
+					data_dict[p]["end_location_id"] = location_id
+
+					#print data_dict[p]
+					#if it has time and price then store it
+					if "expected_wait_time" in data_dict[p] and "surge_multiplier" in data_dict[p]:
+						#print data_dict[p]
+						fileWriter.writerow([data_dict[p]["timestamp"], data_dict[p]["surge_multiplier"], data_dict[p]["expected_wait_time"], data_dict[p]["product_type"], data_dict[p]["low_estimate"], data_dict[p]["high_estimate"], data_dict[p]["start_location_id"], data_dict[p]["end_location_id"]])
+			except TypeError as e:
+				print e
+
+		except Exception as e:
+			print "The response at " + str(aslocaltimestr(datetime.utcnow())) + " was: "
+			print json_response
+			print res.content
+			print e
+
+	return it_responded
+
+
+# Create a scheduler to trigger every N seconds
+# http://apscheduler.readthedocs.org/en/3.0/userguide.html#code-examples
+logging.basicConfig()
+scheduler = BackgroundScheduler()
+scheduler.add_job(gather_loop, 'interval', seconds = time_interval)
+scheduler.start()
+
+while True:
+	time.sleep(1)
 
 
 tokennum = 0
